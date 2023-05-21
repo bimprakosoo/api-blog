2883d260875b30941cea1a34120e81fa84793695
Bimo Prakoso <biimoprakosoo@gmail.com>
2023-05-21 22:55:02 +0700

    Add data user in result API login and place posts show API outside auth

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -43,7 +43,7 @@ class AuthController extends Controller
       if (Auth::attempt($credentials)) {
         $user = Auth::user();
         $token = $user->createToken('API_blog')->plainTextToken;
-        return response()->json(['token' => $token], 200);
+        return response()->json(['token' => $token, 'user' => $user], 200);
       } else {
         return response()->json(['error' => 'Invalid credentials'], 401);
       }
--- routes/api.php
+++ routes/api.php
@@ -19,6 +19,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
+Route::get('post/{id?}', [PostController::class, 'show']);
 
 Route::middleware('auth:sanctum')->group(function () {
   Route::get('user', [AuthController::class, 'getUser']);
@@ -38,7 +39,6 @@ Route::middleware('auth:sanctum')->group(function () {
   
   //Route for post
   Route::post('post/add', [PostController::class, 'store']);
-  Route::get('post/{id?}', [PostController::class, 'show']);
   Route::put('post/edit/{id}', [PostController::class, 'edit']);
   Route::delete('post/delete/{id}', [PostController::class, 'delete']);
   Route::post('post/comment', [PostController::class, 'comment']);
